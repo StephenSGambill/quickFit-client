e1681ffce22bae131cb42fe022f2937f19e770f4
Stephen Gambill <stephensgambill@gmail.com>
2023-06-15 18:06:59 -0500

    tweaked navbar to that links will change on hover

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,25 +8,30 @@ export const NavBar = () => {
 
         <ul className="navbar bg-gray-600 p-5 h-20 text-gray-50 shadow-md fixed top-0 left-0 right-0">
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/about">About</Link>
+                <Link
+                    className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2"
+                    to="/about"
+                >
+                    About
+                </Link>
             </li>
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/profile">Profile</Link>
+                <Link className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2" to="/profile">Profile</Link>
             </li>
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/workouts">Workouts</Link>
+                <Link className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2" to="/workouts">Workouts</Link>
             </li>
 
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/exercises">Exercises</Link>
+                <Link className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2" to="/exercises">Exercises</Link>
             </li>
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/members">Members</Link>
+                <Link className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2" to="/members">Members</Link>
             </li>
             {
                 localStorage.getItem("qfs_user")
                     ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
+                        <Link className="navbar__link transition-all duration-200 transform hover:font-extrabold hover:translate-x-2" to="" onClick={() => {
                             localStorage.removeItem("qfs_user")
                             navigate("/", { replace: true })
                         }}>Logout</Link>
